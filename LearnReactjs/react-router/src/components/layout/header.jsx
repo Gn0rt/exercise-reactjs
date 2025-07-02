97914e978c379059bbd4295d21424efb6f9ed945
Bui The Trong <trongbuithe06@gmail.com>
2025-07-02 16:10:11 +0700

    NavLink

--- LearnReactjs/react-router/src/components/layout/header.jsx
+++ LearnReactjs/react-router/src/components/layout/header.jsx
@@ -1,11 +1,11 @@
-import { Link } from 'react-router';
+import { Link, NavLink } from 'react-router';
 import './header.css';
 const Header = () => {
     return (
         <ul>
-            <li><Link className="active" to="/">Home</Link></li>
-            <li><Link to="/users">Users</Link></li>
-            <li><Link to="/products">Product</Link></li>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/users">Users</NavLink></li>
+            <li><NavLink to="/products">Product</NavLink></li>
         </ul>
 
     );
