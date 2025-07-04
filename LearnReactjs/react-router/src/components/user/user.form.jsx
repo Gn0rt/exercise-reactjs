1d7a4d4d63ed71791d3aa6a69cb55f8fe27beb09
Bui The Trong <trongbuithe06@gmail.com>
2025-07-04 14:41:50 +0700

    cmt

--- LearnReactjs/react-router/src/components/user/user.form.jsx
+++ LearnReactjs/react-router/src/components/user/user.form.jsx
@@ -1,28 +1,58 @@
 import { Input } from "antd";
 import { Button, Flex } from 'antd';
+import { useState } from "react";
 
 const UserForm = () => {
+    const [fullname, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phone, setPhone] = useState("");
+
+    const handleClick = () => {
+        console.log("clicked")
+    }
     return (
         <div className="user-form" style={{ margin: "20px 0" }}>
             <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
                 <div>
                     <span>FullName</span>
-                    <Input />
+                    <Input
+                        value={fullname}
+                        onChange={(e) => setFullName(e.target.value)}
+                        placeholder="Fullname..."
+                    />
                 </div>
                 <div>
                     <span>Email</span>
-                    <Input />
+                    <Input
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Email..."
+
+                    />
                 </div>
                 <div>
                     <span>Password</span>
-                    <Input.Password />
+                    <Input.Password
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Password..."
+
+                    />
                 </div>
                 <div>
                     <span>Phone number</span>
-                    <Input />
+                    <Input
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                        placeholder="Phone number..."
+                    />
                 </div>
                 <div>
-                    <Button type="primary">Create User</Button>
+                    <Button
+                        type="primary"
+                        onClick={handleClick}
+                    >Create User</Button>
                 </div>
             </div>
         </div>
