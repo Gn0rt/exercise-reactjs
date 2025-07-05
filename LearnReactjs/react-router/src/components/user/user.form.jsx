cdd19cf16b4d84403a77096b9e143089f05ae005
Bui The Trong <trongbuithe06@gmail.com>
2025-07-05 16:29:08 +0700

    interceptor

--- LearnReactjs/react-router/src/components/user/user.form.jsx
+++ LearnReactjs/react-router/src/components/user/user.form.jsx
@@ -1,5 +1,4 @@
-import { Input, notification } from "antd";
-import { Button, Flex } from 'antd';
+import { Input, Button, notification } from "antd";
 import { useState } from "react";
 import { createUserApi } from "../../services/api.service";
 
@@ -9,63 +8,67 @@ const UserForm = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [phone, setPhone] = useState("");
+    const [api, contextHolder] = notification.useNotification();
 
     const handleClick = async () => {
-        const respone = await createUserApi(fullName, email, password, phone);
-        if (respone.data && respone.data.data) {
-            notification.success({
-                message: "Create user",
-                description: "Tạo user thành công"
-            })
-        }
+        const response = await createUserApi(fullName, email, password, phone);
+        if (response.data) {
+            api["success"]({
+                message: 'Create user',
+                description: 'Tạo user thành công',
+            });
+        };
 
-        console.log(">>> Result: ", respone.data.data);
+        console.log(">>> Result: ", response.data.data);
     }
     return (
-        <div className="user-form" style={{ margin: "20px 0" }}>
-            <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
-                <div>
-                    <span>FullName</span>
-                    <Input
-                        value={fullName}
-                        onChange={(e) => setFullName(e.target.value)}
-                        placeholder="Fullname..."
-                    />
-                </div>
-                <div>
-                    <span>Email</span>
-                    <Input
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        placeholder="Email..."
+        <>
+            {contextHolder}
+            <div className="user-form" style={{ margin: "20px 0" }}>
+                <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
+                    <div>
+                        <span>FullName</span>
+                        <Input
+                            value={fullName}
+                            onChange={(e) => setFullName(e.target.value)}
+                            placeholder="Fullname..."
+                        />
+                    </div>
+                    <div>
+                        <span>Email</span>
+                        <Input
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder="Email..."
 
-                    />
-                </div>
-                <div>
-                    <span>Password</span>
-                    <Input.Password
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        placeholder="Password..."
+                        />
+                    </div>
+                    <div>
+                        <span>Password</span>
+                        <Input.Password
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Password..."
 
-                    />
-                </div>
-                <div>
-                    <span>Phone number</span>
-                    <Input
-                        value={phone}
-                        onChange={(e) => setPhone(e.target.value)}
-                        placeholder="Phone number..."
-                    />
-                </div>
-                <div>
-                    <Button
-                        type="primary"
-                        onClick={handleClick}
-                    >Create User</Button>
+                        />
+                    </div>
+                    <div>
+                        <span>Phone number</span>
+                        <Input
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                            placeholder="Phone number..."
+                        />
+                    </div>
+                    <div>
+                        <Button
+                            type="primary"
+                            onClick={handleClick}
+                        >Create User</Button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 export default UserForm;
