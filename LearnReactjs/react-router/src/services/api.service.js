e88b7e2e6b66b71ab0d2aa2ceb9f3a5fabd00490
Bui The Trong <trongbuithe06@gmail.com>
2025-07-07 16:49:38 +0700

    lifecycle
    mount - update - unmount

--- LearnReactjs/react-router/src/services/api.service.js
+++ LearnReactjs/react-router/src/services/api.service.js
@@ -11,5 +11,9 @@ const createUserApi = (fullName, email, password, phone) => {
     }
     return axios.post(URL_BACKEND, data);
 }
+const fetchAllUserApi = () => {
+    const URL_BACKEND = "/api/v1/user";
 
-export { createUserApi }
+    return axios.get(URL_BACKEND);
+}
+export { createUserApi, fetchAllUserApi }
