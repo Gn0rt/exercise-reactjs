6eefed53eaf4820428d4e6ed237e9a386b1d6784
Bui The Trong <trongbuithe06@gmail.com>
2025-07-15 18:42:22 +0700

    preview image
    khi chọn ạnh sẽ hiển thị preview (chế độ xem trước)

--- LearnReactjs/react-router/src/components/user/view.user.detail.jsx
+++ LearnReactjs/react-router/src/components/user/view.user.detail.jsx
@@ -2,12 +2,27 @@ import { useState, useEffect } from 'react';
 import { Button, Drawer } from 'antd';
 const ViewUserDetails = (props) => {
     const { isModalViewOpen, setIsModalViewOpen, dataUser, setDataUser } = props;
-
+    const [selectedFile, setSelectedFile] = useState(null)
+    const [preview, setPreview] = useState(null)
 
     const resetAndCloseModal = () => {
         setIsModalViewOpen(false);
         setDataUser(null);
     }
+    const handleOnChangeFile = (e) => {
+        if (!e.target.files || e.target.files.length === 0) {
+            setSelectedFile(null);
+            setPreview(null);
+            return
+        }
+        // I've kept this example simple by using the first image instead of multiple
+        const file = e.target.files[0];
+        if (file) {
+            setSelectedFile(file);
+            setPreview(URL.createObjectURL(file));
+        }
+    }
+    console.log("file: ", preview);
     return (
         <>
             <Drawer
@@ -23,11 +38,17 @@ const ViewUserDetails = (props) => {
                         <p style={{ margin: "10px 0" }}><span style={{ fontWeight: "bold" }}>Full Name:</span> {dataUser.fullName}</p>
                         <p style={{ margin: "10px 0" }}><span style={{ fontWeight: "bold" }}>Email:</span> {dataUser.email}</p>
                         <p style={{ margin: "10px 0" }}><span style={{ fontWeight: "bold" }}>Phone:</span> {dataUser.phone}</p>
-                        <div>
-                            <p>Avatar</p>
+                        <br />
+                        <p>Avatar</p>
+                        <div style={{
+                            marginTop: "10px",
+                            height: "100px", width: "150px",
+                            border: "1px solid #ccc",
+                        }}>
                             <img
-                                height={150}
-                                width={150}
+                                style={{
+                                    height: "100%", width: "100%", objectFit: "contain",
+                                }}
                                 src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataUser.avatar}`} alt="" />
                         </div>
                         <div>
@@ -44,8 +65,27 @@ const ViewUserDetails = (props) => {
                             >
                                 Upload Avatar
                             </label>
-                            <input type="file" hidden id='btnUpload' />
+                            <input type="file" hidden id='btnUpload'
+                                onChange={handleOnChangeFile}
+                            />
                         </div>
+
+                        {preview &&
+
+                            <div style={{
+                                marginTop: "10px",
+                                height: "100px", width: "150px",
+                                border: "1px solid #ccc",
+                            }}>
+                                <img
+                                    style={{
+                                        height: "100%", width: "100%", objectFit: "contain",
+                                    }}
+                                    src={preview} alt="" />
+                            </div>
+                        }
+
+
                         {/* <Button type='primary'>Upaload Avatar</Button> */}
                     </>
                     :
