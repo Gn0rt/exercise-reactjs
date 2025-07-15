1836e0e4c6d6c3650340618b98649f6e0c32dc75
Bui The Trong <trongbuithe06@gmail.com>
2025-07-15 17:43:27 +0700

    Avatar
    Hiển thị avatar user

--- LearnReactjs/react-router/src/components/user/view.user.detail.jsx
+++ LearnReactjs/react-router/src/components/user/view.user.detail.jsx
@@ -11,6 +11,7 @@ const ViewUserDetails = (props) => {
     return (
         <>
             <Drawer
+                width={"40vw"}
                 title="Chi tiết User"
                 closable={{ 'aria-label': 'Close Button' }}
                 onClose={() => resetAndCloseModal()}
@@ -22,6 +23,30 @@ const ViewUserDetails = (props) => {
                         <p style={{ margin: "10px 0" }}><span style={{ fontWeight: "bold" }}>Full Name:</span> {dataUser.fullName}</p>
                         <p style={{ margin: "10px 0" }}><span style={{ fontWeight: "bold" }}>Email:</span> {dataUser.email}</p>
                         <p style={{ margin: "10px 0" }}><span style={{ fontWeight: "bold" }}>Phone:</span> {dataUser.phone}</p>
+                        <div>
+                            <p>Avatar</p>
+                            <img
+                                height={150}
+                                width={150}
+                                src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataUser.avatar}`} alt="" />
+                        </div>
+                        <div>
+                            <label htmlFor='btnUpload'
+                                style={{
+                                    display: "block",
+                                    width: "fit-content",
+                                    marginTop: "15px",
+                                    padding: "5px 10px",
+                                    background: "orange",
+                                    borderRadius: "5px",
+                                    cursor: "pointer",
+                                }}
+                            >
+                                Upload Avatar
+                            </label>
+                            <input type="file" hidden id='btnUpload' />
+                        </div>
+                        {/* <Button type='primary'>Upaload Avatar</Button> */}
                     </>
                     :
                     <>
